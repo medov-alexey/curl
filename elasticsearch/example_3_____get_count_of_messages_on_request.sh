d4c9e2ddc20a5d0c8a7e15206a319bc2c659ead4
Alexey Medov <medov.alexey@gmail.com>
2021-05-06 13:49:44 +0000

    fix

--- elasticsearch/example_3_____get_count_of_messages_on_request.sh
+++ elasticsearch/example_3_____get_count_of_messages_on_request.sh
@@ -27,7 +27,7 @@ execute_file="/tmp/request.sh"
 
 #--------------------------------------------
 
-echo "curl -s -k -XGET -H "Content-Type: application/json" "$URL/$INDEX/_count" -d '{"query": {"bool": {"must": {"query_string": {"query": "$QUERY"}},"filter": {"bool": {"must": {"range": {"$TIMESTAMP_FIELD": {"gt":"$START_TIME", "lte":"$END_TIME" } } } } } } } }' $CRED | jq '.count'" > $execute_file
+echo "curl -s -k -XGET -H \"Content-Type: application/json\" \"$URL/$INDEX/_count\" -d '{\"query\": {\"bool\": {\"must\": {\"query_string\": {\"query\": \"$QUERY\"}},\"filter\": {\"bool\": {\"must\": {\"range\": {\"$TIMESTAMP_FIELD\": {\"gt\":\"$START_TIME\", \"lte\":\"$END_TIME\" } } } } } } } }' $CRED | jq '.count' " > $execute_file
 
 chmod +x $execute_file && sh $execute_file
 
@@ -36,5 +36,5 @@ chmod +x $execute_file && sh $execute_file
 
 example_file="/tmp/example_of_request.txt"
 
-echo "curl -s -k -XGET -H "Content-Type: application/json" "https://elastic.example.com/index_123/_count" -d '{"query": {"bool": {"must": {"query_string": {"query": "Type:Nginx AND yandex.ru/market/id3434" } },"filter": {"bool": {"must": {"range": {"timestamp": {"gt":"2020-01-30 15:33:10.543", "lte":"now", "time_zone":"+03:00" } } } } } } } }' --user admin:superpassword123" > $example_file
+echo "curl -s -k -XGET -H \"Content-Type: application/json\" \"https://elastic.example.com/index_123/_count\" -d '{\"query\": {\"bool\": {\"must\": {\"query_string\": {\"query\": \"Type:Nginx AND yandex.ru/market/id3434\" } },\"filter\": {\"bool\": {\"must\": {\"range\": {\"timestamp\": {\"gt\":\"2020-01-30 15:33:10.543\", \"lte\":\"now\", \"time_zone\":\"+03:00\" } } } } } } } }' --user admin:superpassword123" > $example_file
 
