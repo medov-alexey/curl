002a56ea97509c4e6ec7e9a0a4e9ae62d0c3bea6
Medov Alexey <medov.alexey@gmail.com>
2021-05-17 14:58:22 +0300

    fix

--- rancher/1.6.30/restart_service.sh
+++ rancher/1.6.30/restart_service.sh
@@ -2,10 +2,11 @@
 
 # Example
 #
-# RANCHER_URL=192.168.168.80 RANCHER_ACCESS_KEY=8B6C872C9CB615961F60 RANCHER_SECRET_KEY=9XuB1WHMvBY3WSyp2Zk9Lwc1MdAfTnHa93ci92Dw PROJECT_ID=1a5 STACK_NAME=MyStack SERVICE_NAME=Nginx ./restart_service.sh
+# RANCHER_PROTOCOL=http RANCHER_URL=192.168.168.80 RANCHER_ACCESS_KEY=8B6C872C9CB615961F60 RANCHER_SECRET_KEY=9XuB1WHMvBY3WSyp2Zk9Lwc1MdAfTnHa93ci92Dw PROJECT_ID=1a5 STACK_NAME=MyStack SERVICE_NAME=Nginx ./restart_service.sh
 
 RANCHER_ACCESS_KEY="$RANCHER_ACCESS_KEY"
 RANCHER_SECRET_KEY="$RANCHER_SECRET_KEY"
+RANCHER_PROTOCOL="$RANCHER_PROTOCOL"
 RANCHER_URL="$RANCHER_URL"
 PROJECT_ID="$PROJECT_ID"
 
@@ -26,11 +27,11 @@ cat /dev/null > $TEMP_FILE_2
 #-------------------------------
 # STEP 1
 
-count_stacks=$(curl -f -s -S -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' http://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stacks | jq -r '.data[].name' | wc -l)
+count_stacks=$(curl -f -s -S -k -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' $RANCHER_PROTOCOL://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stacks | jq -r '.data[].name' | wc -l)
 
 for ((i=0; i<$count_stacks; i++))
 do
-   cmd=$(echo "curl -f -s -S -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' http://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stacks | jq -r '.data[$i].name,.data[$i].id' ")
+   cmd=$(echo "curl -f -s -S -k -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' $RANCHER_PROTOCOL://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stacks | jq -r '.data[$i].name,.data[$i].id' ")
    echo $(eval $cmd) >> $TEMP_FILE_1
 done
 
@@ -39,11 +40,11 @@ STACK_ID=$(cat $TEMP_FILE_1 | grep $STACK_NAME | awk '{print $2}')
 #-------------------------------
 # STEP 2
 
-count_services=$(curl -f -s -S -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' http://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stack/$STACK_ID/services | jq '.data[] .name' | wc -l)
+count_services=$(curl -f -s -S -k -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' $RANCHER_PROTOCOL://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stack/$STACK_ID/services | jq '.data[] .name' | wc -l)
 
 for ((i=0; i<$count_services; i++))
 do
-   cmd=$(echo "curl -f -s -S -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' http://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stack/$STACK_ID/services | jq -r '.data[$i] | .name,.id' ")
+   cmd=$(echo "curl -f -s -S -k -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" -X GET -H 'Content-Type: application/json' $RANCHER_PROTOCOL://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/stack/$STACK_ID/services | jq -r '.data[$i] | .name,.id' ")
    echo $(eval $cmd) >> $TEMP_FILE_2
 done
 
@@ -54,7 +55,7 @@ SERVICE_ID=$(cat $TEMP_FILE_2 | grep $SERVICE_NAME | awk '{print $2}')
 
 echo "Restarting..."
 
-curl -f -s -S -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" \
+curl -f -s -S -k -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" \
 -X POST \
 -H 'Content-Type: application/json' \
 -d '{
@@ -62,7 +63,7 @@ curl -f -s -S -u "$RANCHER_ACCESS_KEY:$RANCHER_SECRET_KEY" \
                 "batchSize": 1,
                 "intervalMillis": 2000
         }
-}' http://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/services/$SERVICE_ID?action=restart > /dev/null 2>&1
+}' $RANCHER_PROTOCOL://$RANCHER_URL/v2-beta/projects/$PROJECT_ID/services/$SERVICE_ID?action=restart > /dev/null 2>&1
 
 #-------------------------------
 
